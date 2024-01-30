157123cea5f6e7f8852e7d91a6115aaa55f656f0
Laercio Rios <contact@laerciorios.com>
2024-01-30 19:59:20 -0300

    feat: add content in home

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,29 @@
 "use client";
-import { Main } from "./style";
+import { Content, Main, Subtitle, Title } from "./style";
+import { semesters } from "./api/db.json";
 
 export default function Home() {
-  return <Main></Main>;
+  const today = new Date();
+  const daysToEnd = semesters
+    .map((semester) =>
+      Math.ceil(
+        (new Date(semester.end_at).getTime() - today.getTime()) /
+          (1000 * 60 * 60 * 24)
+      )
+    )
+    .reduce((min, cur) => {
+      if (cur > 0 && cur < min) {
+        return cur;
+      }
+      return min;
+    }, 999);
+
+  return (
+    <Main>
+      <Content>
+        <Title>{daysToEnd} dias</Title>
+        <Subtitle>para o fim do semestre da UEFS</Subtitle>
+      </Content>
+    </Main>
+  );
 }
--- app/style.ts
+++ app/style.ts
@@ -5,3 +5,21 @@ export const Main = styled("main")`
   width: 100vw;
   height: 100vh;
 `;
+
+export const Content = styled("div")`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+`;
+
+export const Title = styled("h1")`
+  text-transform: uppercase;
+  font-size: 16rem;
+`;
+
+export const Subtitle = styled("h2")`
+  text-transform: uppercase;
+  font-size: 3rem;
+`;
