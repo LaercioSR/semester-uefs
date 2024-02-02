67444210d4f9a00e9fe0187222f2ec409c35518e
Laercio Rios <contact@laerciorios.com>
2024-02-02 15:10:01 -0300

    feat: add post to tweet about days in semester

---
+++ src/server/controller/tweet.ts
@@ -0,0 +1,55 @@
+import { semesterRepository } from "@server/repository/semester";
+import { tweetRepository } from "@server/repository/tweet";
+import { ApiResponseError } from "twitter-api-v2";
+
+function dateMessage(days: number, event: "início" | "fim" = "fim"): string[] {
+  if (days === 0) {
+    return ["Hoje", "acaba o"];
+  }
+  if (days === 1) {
+    return ["Amanhã", "acaba o"];
+  }
+  return [`Faltam ${days} dias`, `para o ${event} do`];
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+async function postTweetSemesterDay(request: Request) {
+  let message = "";
+  try {
+    const daysToEnd = await semesterRepository.getDaysToEndCurrentSemester();
+    const currentSemester = await semesterRepository.getCurrentSemester();
+    const [daysToEndMessage, eventEndMessage] = dateMessage(daysToEnd, "fim");
+    message = `${daysToEndMessage} ${eventEndMessage} semestre ${currentSemester.title} da UEFS`;
+  } catch {
+    const daysToStart = await semesterRepository.getDaysToStartNextSemester();
+    const nextSemester = await semesterRepository.getNextSemester();
+    const [daysToStartMessage, eventStartMessage] = dateMessage(
+      daysToStart,
+      "início"
+    );
+    message = `${daysToStartMessage} ${eventStartMessage} semestre ${nextSemester.title} da UEFS`;
+  }
+
+  message += "\n\nVeja mais em: https://semestreuefs.laerciorios.com/";
+
+  try {
+    await tweetRepository.create(message);
+  } catch (error) {
+    if (error instanceof ApiResponseError) {
+      return new Response(
+        JSON.stringify({
+          error: {
+            message: "Failed to post tweet",
+          },
+        }),
+        { status: error.code }
+      );
+    }
+  }
+
+  return new Response(undefined, { status: 204 });
+}
+
+export const tweetController = {
+  postTweetSemesterDay,
+};
